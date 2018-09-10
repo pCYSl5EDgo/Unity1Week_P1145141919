681ef34768f67288cded2ace7270a11109e9c7a5
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2018-09-10 14:24:36 +0900

    [update]Tweet機能でランクを表現するようにした

--- Assets/MyFolder/Scripts/UIManager/TweetButton.cs
+++ Assets/MyFolder/Scripts/UIManager/TweetButton.cs
@@ -11,11 +11,19 @@ namespace Unity1Week.UI
         public uint KillScore;
         public override void OnPointerClick(PointerEventData eventData)
         {
-#if UNITY_EDITOR
-            Debug.Log(KillScore);
-#else
-            naichilab.UnityRoomTweet.Tweet("p1145141919", $"スコア:{KillScore}", "unity1week", "晩夏の昼の挽歌");
-#endif
+            if (KillScore < 100)
+                naichilab.UnityRoomTweet.Tweet("p1145141919", $"#unity1week ランク：自滅\n「晩夏の昼の挽歌」をプレイ\nスコア:{KillScore}\nhttps://unityroom.com/games/p1145141919");
+            else if (KillScore < 1000)
+                naichilab.UnityRoomTweet.Tweet("p1145141919", $"#unity1week ランク：クソザコナメクジ\n「晩夏の昼の挽歌」をプレイ\nスコア:{KillScore}\nhttps://unityroom.com/games/p1145141919");
+            else if (KillScore < 10000)
+                naichilab.UnityRoomTweet.Tweet("p1145141919", $"#unity1week ランク：クッキー☆\n「晩夏の昼の挽歌」をプレイ\nスコア:{KillScore}\nhttps://unityroom.com/games/p1145141919");
+            else if (KillScore < 50000)
+                naichilab.UnityRoomTweet.Tweet("p1145141919", $"#unity1week ランク：姉貴兄貴姉貴\n「晩夏の昼の挽歌」をプレイ\nスコア:{KillScore}\nhttps://unityroom.com/games/p1145141919");
+            else if (KillScore < 100000)
+                naichilab.UnityRoomTweet.Tweet("p1145141919", $"#unity1week ランク：ナアル地獄の脱獄皇\n「晩夏の昼の挽歌」をプレイ\nスコア:{KillScore}\nhttps://unityroom.com/games/p1145141919");
+            else if (KillScore < 114514)
+                naichilab.UnityRoomTweet.Tweet("p1145141919", $"#unity1week ランク：ニアミス先輩\n「晩夏の昼の挽歌」をプレイ\nスコア:{KillScore}\nhttps://unityroom.com/games/p1145141919");
+            else naichilab.UnityRoomTweet.Tweet("p1145141919", $"#unity1week ランク：異能生存体\n「晩夏の昼の挽歌」をプレイ\nスコア:{KillScore}\nhttps://unityroom.com/games/p1145141919");
         }
     }
 }
