9d35e5a354455fb8f060219b8fd481e48cef7071
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2018-09-11 11:52:15 +0900

    [update]雪攻撃とプレイヤーの衝突判定とダメージ処理

--- Assets/MyFolder/Scripts/ECS/System/Hit/SnowPlayerHit.cs
+++ Assets/MyFolder/Scripts/ECS/System/Hit/SnowPlayerHit.cs
@@ -1,11 +1,10 @@
 ﻿using Unity.Entities;
-using Unity.Mathematics;
 using Unity.Collections;
-using Unity.Collections.LowLevel.Unsafe;
 using Unity.Transforms;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 namespace Unity1Week
 {
@@ -14,23 +13,79 @@ namespace Unity1Week
     public sealed class SnowPlayerHitCheckSystem : ComponentSystem
     {
         private readonly float rangeSquared;
+        private readonly float damageRatio;
+        private readonly ReactiveProperty<uint> killScore;
         private readonly Entity player;
         private readonly Action playSoundEffect;
         private readonly NativeMultiHashMap<int, DecidePositionHashCodeSystem.Tuple> snowHashCodes;
         private readonly NativeMultiHashMap<int, DecidePositionHashCodeSystem.Tuple> playerBulletHashCodes;
+        private readonly HashSet<int> allPositionHashCodes;
+        private readonly HashSet<DecidePositionHashCodeSystem.Tuple> toDestroy = new HashSet<DecidePositionHashCodeSystem.Tuple>();
 
-        public SnowPlayerHitCheckSystem(Entity player, float radius, NativeMultiHashMap<int, DecidePositionHashCodeSystem.Tuple> snowHashCodes, NativeMultiHashMap<int, DecidePositionHashCodeSystem.Tuple> playerBulletHashCodes, Action playSoundEffect)
+        public SnowPlayerHitCheckSystem(Entity player, float damageRatio, UniRx.ReactiveProperty<uint> killScore, float radius, NativeMultiHashMap<int, DecidePositionHashCodeSystem.Tuple> snowHashCodes, NativeMultiHashMap<int, DecidePositionHashCodeSystem.Tuple> playerBulletHashCodes, HashSet<int> allPositionHashCodes, Action playSoundEffect)
         {
             this.rangeSquared = radius * radius;
+            this.damageRatio = damageRatio;
+            this.killScore = killScore;
             this.player = player;
             this.playSoundEffect = playSoundEffect;
             this.snowHashCodes = snowHashCodes;
-			this.playerBulletHashCodes = playerBulletHashCodes;
+            this.playerBulletHashCodes = playerBulletHashCodes;
+            this.allPositionHashCodes = allPositionHashCodes;
         }
 
         protected override void OnUpdate()
         {
-            throw new NotImplementedException();
+            toDestroy.Clear();
+            var manager = EntityManager;
+            var buf = PostUpdateCommands;
+            var playerPosition = manager.GetComponentData<Position>(player).Value;
+            var playerSettings = manager.GetComponentData<PlayerSettings>(player);
+            var playerX = playerPosition.x;
+            var playerY = playerPosition.y;
+            var damage = damageRatio * Time.deltaTime * killScore.Value;
+            var temperatureDelta = damageRatio * Time.deltaTime;
+            DecidePositionHashCodeSystem.Tuple snowItem, playerBulletItem;
+            NativeMultiHashMapIterator<int> snowIt, playerBulletIt;
+            foreach (var key in allPositionHashCodes)
+            {
+                if (!snowHashCodes.TryGetFirstValue(key, out snowItem, out snowIt)) continue;
+                var diffX = playerX - snowItem.Position.x;
+                var diffY = playerY - snowItem.Position.y;
+                if (diffX * diffX + diffY * diffY <= rangeSquared)
+                {
+                    playerSettings.Life -= damage;
+                    playerSettings.Temperature -= temperatureDelta;
+                    toDestroy.Add(snowItem);
+                    goto NEXT;
+                }
+                if (!playerBulletHashCodes.TryGetFirstValue(key, out playerBulletItem, out playerBulletIt)) continue;
+                toDestroy.Add(snowItem);
+                toDestroy.Add(playerBulletItem);
+                while (playerBulletHashCodes.TryGetNextValue(out playerBulletItem, ref playerBulletIt))
+                    toDestroy.Add(playerBulletItem);
+                NEXT:
+                while (snowHashCodes.TryGetNextValue(out snowItem, ref snowIt))
+                {
+                    diffX = playerX - snowItem.Position.x;
+                    diffY = playerY - snowItem.Position.y;
+                    if (diffX * diffX + diffY * diffY <= rangeSquared)
+                    {
+                        playerSettings.Life -= damage;
+                        playerSettings.Temperature -= temperatureDelta;
+                        toDestroy.Add(snowItem);
+                        continue;
+                    }
+                    if (!playerBulletHashCodes.TryGetFirstValue(key, out playerBulletItem, out playerBulletIt)) continue;
+                    toDestroy.Add(snowItem);
+                    toDestroy.Add(playerBulletItem);
+                    while (playerBulletHashCodes.TryGetNextValue(out playerBulletItem, ref playerBulletIt))
+                        toDestroy.Add(playerBulletItem);
+                }
+            }
+            foreach (var item in toDestroy)
+                buf.DestroyEntity(item.Entity);
+            manager.SetComponentData(player, playerSettings);
         }
     }
 }
